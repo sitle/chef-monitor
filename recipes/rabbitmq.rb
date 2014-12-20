80b66fa4fa672e2b6065f708013dbdaf55022a36
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-12-20 11:44:05 -1000

    Support de rabbitmq.

---
+++ recipes/rabbitmq.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: chef-monitor
+# Recipe:: rabbitmq
+#
+# Copyright (C) 2014 Leonard TAVAE
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'sensu::rabbitmq'
+
+rabbitmq_user 'admin' do
+  password 'password'
+  action :add
+end
+
+rabbitmq_user 'admin' do
+  vhost '/'
+  permissions '.* .* .*'
+  action :set_permissions
+end
+
+rabbitmq_user 'admin' do
+  tag 'administrator'
+  action :set_tags
+end
