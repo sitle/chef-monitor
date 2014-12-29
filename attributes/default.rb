0b13cbca71f614492485f4769b30986cb0881362
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-12-29 13:34:46 -1000

    Prise en compte des rôles dynamiquement.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,14 @@ default['chef-monitor']['rabbitmq_admin'] = 'admin'
 default['chef-monitor']['rabbitmq_admin_password'] = 'password'
 default['chef-monitor']['rabbitmq_usercheck'] = 'monitor'
 default['chef-monitor']['rabbitmq_usercheck_password'] = 'password'
+default['chef-monitor']['rabbitmq_role'] = 'monitor-queue'
 
 ## Configuration de redis
 #
 default['chef-monitor']['redis_host'] = 'localhost'
 default['chef-monitor']['redis_port'] = '6379'
+default['chef-monitor']['redis_role'] = 'monitor-queue'
 
+## Configuration uchiwa
+#
+default['chef-monitor']['webui_role'] = 'monitor-webui'
